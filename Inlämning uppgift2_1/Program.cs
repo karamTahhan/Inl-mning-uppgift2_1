db8d3093d85b7da6a3ab3c218e374cb9dba6401f
karamTahhan <kata07001@utb.vaxjo.se>
2024-10-03 13:42:54 +0200

    Add project files.

---
+++ Inlämning uppgift2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace uppgift2_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är din favorit bok?");
+            string X=Console.ReadLine();
+            Console.WriteLine(X);
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+}
